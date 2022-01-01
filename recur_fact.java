de0770198e47f28fd9002c342052bbb7c7635779
Rakesh Chaudhary <93024101+Chaudhary1rakesh@users.noreply.github.com>
2022-01-01 23:14:19 +0530

    Add files via upload

---
+++ recur_fact.java
@@ -0,0 +1,20 @@
+public class recur_fact {
+   
+    static public int factorial(int n){
+        if(n==0||n==1)
+        {
+            return 1;
+        }
+        else
+        {
+return n*factorial(n-1);
+        }
+       
+    }
+    public static void main(String[] args) {
+      int n=5;
+      System.out.println(factorial(n));
+    }
+    
+    
+}
